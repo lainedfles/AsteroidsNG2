27b1d4b78810ffa7e0caa86c86b8f851de772f29
Self Denial <selfdenial@pm.me>
2025-01-09 20:26:42 -0700

    Update ignore, add constants, main, requirements

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((0, 0, 0))
+
+        pygame.display.flip()
+
+try:
+    main()
+except IOError as e:
+    if e.errno == errno.EPIPE:
+        # Support pipeline
+        pass
